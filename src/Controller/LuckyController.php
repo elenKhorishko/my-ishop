2b5a3d448488cc5b1613fec12686e8ce7ae7c1a5
elena <elena.dnepr.2007@gmail.com>
2017-12-01 21:25:39 +0200

    Initial commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 01.12.2017
+ * Time: 20:46
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     *
+     * @return Response
+     */
+    public function number(){
+        $number = mt_rand(1, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+
+    }
+}
