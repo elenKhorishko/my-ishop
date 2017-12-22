0b5fb0948d0b7b8f2a970825c4219e3af8f27475
elena <elena.dnepr.2007@gmail.com>
2017-12-22 21:23:19 +0200

    Commit lesson_23

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -35,8 +35,9 @@ class MenuBuilder
     {
         $menu = $this->factory->createItem('root');
 
-        $menu->addChild('Главная', ['route' => 'about_show']);
+        $menu->addChild('Главная', ['route' => 'homepage']);
         $catalogueMenu = $menu->addChild('Каталог', ['route' => 'categoies_list']);
+        $catalogueMenu->setExtra('dropdown', true);
 
         foreach ($this->catalogueService->getTopCategories() as $category) {
             $catalogueMenu->addChild($category->getName(), [
