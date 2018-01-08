b979c8f5b038acf3082dd72fabdd445c0142ccee
elenKhorishko <elena.dnepr.2007@gmail.com>
2018-01-08 19:26:32 +0200

    Commit Lesson_25_Finish

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+
 ];
